19eb9c67e815110fc83f7a41494ae4ce85a2544e
Chrystian Ferreira <chrysterafrz@hotmail.com>
2021-04-05 11:52:50 -0300

    Aula 03. Refatorando e evoluindo o código

---
+++ src/components/BarraNavegacaoQuandoLogado.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/gerentes" class="nav-link">
+              Gerentes
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a
+             class="nav-link"
+              href="#"
+              @click.prevent="efetuarLogout">Logout</a>
+          </li>
+        </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    efetuarLogout() {
+      this.$store.commit('DESLOGAR_USUARIO')
+      this.$router.push({name: 'login'});
+    }
+  }  
+}
+</script>
+
+<style>
+
+</style>
